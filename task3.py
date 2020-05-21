02b5d434c8c3a0c0a85ed49a7ec29ad606ecf08f
knavka <knavka13@gmail.com>
2020-05-21 20:29:00 +0300

    add task3

---
+++ task3.py
@@ -0,0 +1,10 @@
+def mult(x,y):
+    return x*y
+def multExercise ():
+    print("Input first multiplier:")
+    x = int(input())
+    print("Input second multiplier:")
+    y = int(input())
+    print("Result of multiplication is "+str(mult(x,y)))
+
+multExercise()
